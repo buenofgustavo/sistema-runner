46a0c836d417a399c087725ddcf94316da07bb21
Gustavo Bueno <buenofgustavo@gmail.com>
2026-03-18 19:30:55 -0300

    feat: initial commit assinador

---
+++ assinador/src/main/java/runner/assinador/AssinadorApplication.java
@@ -0,0 +1,13 @@
+package runner.assinador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssinadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssinadorApplication.class, args);
+	}
+
+}
