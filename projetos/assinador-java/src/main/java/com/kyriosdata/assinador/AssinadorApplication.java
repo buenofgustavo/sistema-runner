cdf565f2800ff411ef0f898512848e4fe828a7eb
Gustavo Bueno <buenofgustavo@gmail.com>
2026-04-01 21:51:26 -0300

    feat: cria main no assinador-java

---
+++ projetos/assinador-java/src/main/java/com/kyriosdata/assinador/AssinadorApplication.java
@@ -0,0 +1,65 @@
+package com.kyriosdata.assinador;
+
+import com.kyriosdata.assinador.domain.SignatureRequest;
+import com.kyriosdata.assinador.domain.SignatureResponse;
+
+import java.util.Locale;
+
+public final class AssinadorApplication {
+
+    private AssinadorApplication() {
+    }
+
+    public static void main(String[] args) {
+        SignatureService signatureService = new FakeSignatureService();
+
+        if (args.length == 0) {
+            printUsage();
+            return;
+        }
+
+        String command = args[0].toLowerCase(Locale.ROOT);
+        switch (command) {
+            case "sign" -> sign(signatureService, args);
+            case "validate" -> validate(signatureService, args);
+            default -> printUsage();
+        }
+    }
+
+    private static void sign(SignatureService signatureService, String[] args) {
+        if (args.length < 2) {
+            printUsage();
+            return;
+        }
+
+        SignatureRequest request = new SignatureRequest();
+        request.setContent(args[1]);
+
+        printResponse(signatureService.sign(request));
+    }
+
+    private static void validate(SignatureService signatureService, String[] args) {
+        if (args.length < 3) {
+            printUsage();
+            return;
+        }
+
+        SignatureRequest request = new SignatureRequest();
+        request.setContent(args[1]);
+        request.setSignature(args[2]);
+
+        printResponse(signatureService.validate(request));
+    }
+
+    private static void printResponse(SignatureResponse response) {
+        System.out.println("valid=" + response.isValid());
+        System.out.println("signature=" + response.getSignature());
+        System.out.println("message=" + response.getMessage());
+    }
+
+    private static void printUsage() {
+        System.out.println("Uso:");
+        System.out.println("  sign <content>");
+        System.out.println("  validate <content> <signature>");
+    }
+}
